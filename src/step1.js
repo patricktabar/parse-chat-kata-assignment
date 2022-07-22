456b479a97e988fdd26525944cb7aa689a608770
patrick <patrick.tabarelli@gmail.com>
2022-07-22 23:58:42 +0200

    step1 - single sentence

--- src/step1.js
+++ src/step1.js
@@ -1,7 +1,16 @@
-const  parseStep1 = (text) => {
-  return [{
-    test: 'test'
-  }]
+// each piece of the chat is composed by hh:mm:ss, customer/agent name, : and sentence
+
+const parseStep1 = (t) => {
+  const dateLen = 'hh:mm:ss'.length;
+  const date = t.slice(0, dateLen);
+  const t2 = t.slice(dateLen + 1);
+  const type = t2.slice(0, t2.indexOf(':')).trim().toLowerCase();
+  const sentence = t2.slice(t2.indexOf(':') + 2);
+  const mention = t.slice(0, t.indexOf(sentence));
+
+  const res = [{ date, mention, sentence, type }];
+
+  return res;
 }
 
 export default parseStep1;
